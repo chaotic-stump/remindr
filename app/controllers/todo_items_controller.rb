4be957fb85429c2f861a549e7eb6341f69bcbac8
Shaws <seanrichardparker@gmail.com>
2016-06-04 12:02:49 -0600

    adding todo list and items models and controllers

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,15 @@
+class TodoItemsController < ApplicationController
+  before_action :todo_list, excerpt: [:index, :new, :create]
+
+  def idex
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
