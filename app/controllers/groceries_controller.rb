3ed457889302e1926b274e1d1a769cf16a4ad99a
Mara Behles Dean <maraebehles@gmail.com>
2016-06-04 13:31:00 -0600

    groceries working

---
+++ app/controllers/groceries_controller.rb
@@ -0,0 +1,46 @@
+class GroceriesController < ApplicationController
+  before_action :grocery, except: [:index, :new, :create]
+
+  def index
+  	@groceries = Grocery.all
+  end
+
+  def new
+  	@grocery = Grocery.new
+  end
+
+  def create
+  	@grocery = Grocery.new(grocery_params)
+  	if @grocery.save
+  		redirect_to grocery_path(@grocery)
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+    list_name = @grocery.name
+    @grocery.destroy
+    redirect_to groceries_path
+  end
+
+  private
+
+  def grocery_params
+  	params.require(:grocery).permit(:name, :completed)
+  end
+
+  def grocery
+  	@grocery = Grocery.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :bank_items
+  resources :groceries
 
 
 end
